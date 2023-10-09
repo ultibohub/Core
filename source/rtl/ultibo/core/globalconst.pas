b2a810c12656c3c6a3772ce864f56efdfbf0d188
Ultibo <admin@ultibo.org>
2023-10-10 10:18:11 +1100

    Minor change to default config.txt

--- source/rtl/ultibo/core/globalconst.pas
+++ source/rtl/ultibo/core/globalconst.pas
@@ -50,12 +50,12 @@ interface
 {Global constants}
 const
  {Version constants}
- ULTIBO_RELEASE_DATE             = '6 October 2023';
+ ULTIBO_RELEASE_DATE             = '10 October 2023';
  ULTIBO_RELEASE_NAME             = 'Beetroot';
- ULTIBO_RELEASE_VERSION          = '2.5.733';
+ ULTIBO_RELEASE_VERSION          = '2.5.739';
  ULTIBO_RELEASE_VERSION_MAJOR    = 2;
  ULTIBO_RELEASE_VERSION_MINOR    = 5;
- ULTIBO_RELEASE_VERSION_REVISION = 733;
+ ULTIBO_RELEASE_VERSION_REVISION = 739;
  
  FPC_COMPILER_VERSION = {$I %FPCVERSION%}; {The version of FPC used to compile the RTL}
  
