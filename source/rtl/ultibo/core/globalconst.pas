631c2218510cb4815dbc25843e8764b9692b164c
Ultibo <admin@ultibo.org>
2023-12-13 14:17:18 +1100

    Change to TOSTimestamp after upstream merge

--- source/rtl/ultibo/core/globalconst.pas
+++ source/rtl/ultibo/core/globalconst.pas
@@ -50,12 +50,12 @@ interface
 {Global constants}
 const
  {Version constants}
- ULTIBO_RELEASE_DATE             = '17 November 2023';
+ ULTIBO_RELEASE_DATE             = '13 December 2023';
  ULTIBO_RELEASE_NAME             = 'Beetroot';
- ULTIBO_RELEASE_VERSION          = '2.5.801';
+ ULTIBO_RELEASE_VERSION          = '2.5.825';
  ULTIBO_RELEASE_VERSION_MAJOR    = 2;
  ULTIBO_RELEASE_VERSION_MINOR    = 5;
- ULTIBO_RELEASE_VERSION_REVISION = 801;
+ ULTIBO_RELEASE_VERSION_REVISION = 825;
  
  FPC_COMPILER_VERSION = {$I %FPCVERSION%}; {The version of FPC used to compile the RTL}
  
