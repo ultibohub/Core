160a83d3f9118ba92bc34a054ff0832af8302651
Ultibo <admin@ultibo.org>
2025-05-21 17:34:54 +1000

    Add libmad MPEG library and build information

--- source/rtl/ultibo/core/globalconst.pas
+++ source/rtl/ultibo/core/globalconst.pas
@@ -50,12 +50,12 @@ interface
 {Global constants}
 const
  {Version constants}
- ULTIBO_RELEASE_DATE             = '13 May 2025';
+ ULTIBO_RELEASE_DATE             = '21 May 2025';
  ULTIBO_RELEASE_NAME             = 'Beetroot';
- ULTIBO_RELEASE_VERSION          = '2.6.051';
+ ULTIBO_RELEASE_VERSION          = '2.6.053';
  ULTIBO_RELEASE_VERSION_MAJOR    = 2;
  ULTIBO_RELEASE_VERSION_MINOR    = 6;
- ULTIBO_RELEASE_VERSION_REVISION = 051;
+ ULTIBO_RELEASE_VERSION_REVISION = 053;
  
  FPC_COMPILER_VERSION = {$I %FPCVERSION%}; {The version of FPC used to compile the RTL}
  
